3d18a5620c14ae09b88bb529c90859daf16595e1
ingonoka <ingonoka@icloud.com>
2023-01-16 00:11:36 +0800

    Suppress inspection for  unstable API usage
    Signed-off-by: ingonoka <ingonoka@icloud.com>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,11 +1,13 @@
+@file:Suppress("UnstableApiUsage")
+
 /*
- * Copyright (c) 2023. Ingo Noka
- * This file belongs to project cba9-driver.
- * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
- * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
- *
- */
+* Copyright (c) 2023. Ingo Noka
+* This file belongs to project cba9-driver.
+* This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License.
+* To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/ or send a letter to
+* Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+*
+*/
 
 plugins {
     id("com.android.library")
@@ -27,14 +29,11 @@ android {
     namespace = "com.ingonoka"
 
     compileSdk = project.properties["android_compile_sdk_version"].toString().toInt()
-//    buildToolsVersion android_build_tools_version
 
     defaultConfig {
 
         minSdk = project.properties["android_min_sdk_version"].toString().toInt()
         targetSdk = project.properties["android_target_sdk_version"].toString().toInt()
-//        versionCode = (project.properties["getVersionCode"] as ()->Int)()
-//        versionName = project.version.toString()
         resourceConfigurations.addAll(listOf("en", "de"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -116,7 +115,7 @@ afterEvaluate {
             withType<MavenPublication> {
                 pom {
                     name.set("CBA9 Banknote Acceptor Driver Library")
-                    description.set("A library implementing the SSP protocol for CBA9 Banknopte Acceptors")
+                    description.set("A library implementing the SSP protocol for CBA9 Banknote Acceptors")
                     url.set("https://github.com/ingonoka/cba9-driver")
 
                     issueManagement {
